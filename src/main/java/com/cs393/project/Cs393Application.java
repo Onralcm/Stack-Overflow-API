42caca687efdc76dac2119a57378fc8d063992fe
Tuna Tuncer <kuantuna.tr@gmail.com>
2021-12-16 16:11:30 +0300

    gradle support and spring dependencies added

---
+++ src/main/java/com/cs393/project/Cs393Application.java
@@ -0,0 +1,13 @@
+package com.cs393.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs393Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs393Application.class, args);
+	}
+
+}
